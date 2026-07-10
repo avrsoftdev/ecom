8bfcc9ceb4f9995eab17dd93166842e350e893a6
avrsoftdev <avrsoftdev@gmail.com>
2026-07-10 17:30:01 +0530

    Not Servicable

--- lib/features/common/presentation/pages/maintenance_page.dart
+++ lib/features/common/presentation/pages/maintenance_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MaintenancePage extends StatelessWidget {
   const MaintenancePage({super.key});
@@ -8,6 +9,12 @@ class MaintenancePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF006400),
+        foregroundColor: Colors.white,
+        title: const Text('Bazariyo'),
+        centerTitle: false,
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 32.h),
@@ -15,27 +22,33 @@ class MaintenancePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Icon(
-                Icons.build_circle_outlined,
-                size: 96.sp,
-                color: Theme.of(context).colorScheme.primary,
+              Image.asset(
+                'assets/images/temp_close.png',
+                width: 280.sp,
+                height: 280.sp,
+                fit: BoxFit.contain,
               ),
               SizedBox(height: 32.h),
               Text(
                 'Bazariyo is Not Servicable for now, We will be back soon',
-                style: TextStyle(
-                  fontSize: 24.sp,
+                style: GoogleFonts.poppins(
+                  fontSize: 28.sp,
                   fontWeight: FontWeight.w700,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  height: 1.3,
+                  letterSpacing: 0.5,
                 ),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 24.h),
               Text(
                 'We are currently performing maintenance. Please check back shortly.',
-                style: TextStyle(
+                style: GoogleFonts.poppins(
                   fontSize: 16.sp,
-                  height: 1.5,
+                  height: 1.6,
+                  letterSpacing: 0.3,
                   color: Theme.of(context).textTheme.bodyMedium?.color,
+                  fontWeight: FontWeight.w400,
                 ),
                 textAlign: TextAlign.center,
               ),
